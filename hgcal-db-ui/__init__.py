58b860fb0cc36e3a1b2c3535d25f80067c43ae80
Alexande Becker <a.becker@cern.ch>
2023-03-01 09:23:07 +0100

    fist step of the flask tutorial implemented

--- hgcal-db-ui/__init__.py
+++ hgcal-db-ui/__init__.py
@@ -1,1 +1,21 @@
 __version__ = "0.0.1"
+
+import os
+from flask import Flask
+
+
+def create_app(test_config=None):
+    app: Flask = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+            SECRET_KEY='dev',
+            DATABASE=os.path.join(app.instance_path, 'staging-db.sqlite'),
+    )
+    # Apply configuration to the app
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    os.makedirs(app.instance_path, exist_ok=True)
+
+    return app
